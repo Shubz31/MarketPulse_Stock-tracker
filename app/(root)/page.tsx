c474d8b7c19c2ad02e052652f31aba0f2a844054
Shubz31 <shubham.kadam1971@gmail.com>
2026-04-10 00:54:34 +0530

    Implement home page with TradingView widgets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,7 +19,7 @@ const page = () => {
             height={600}
           />
         </div>
-        <div className='md-col-span xl:col-span-2'>
+        <div className='md:col-span xl:col-span-2'>
           <TradingViewWidget 
             title='Stock Heatmap'
             scriptUrl={`${scriptUrl}stock-heatmap.js`}
